60771f8036213630cc1ab03bcded60c370854b51
ryanoh671 <ryanoh671@gmail.com>
2023-06-15 10:29:32 -0700

    starting lab part 5

--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,7 @@
-from django.shortcuts import render
-
-# Baby step - Usually a Model is used
-finches = [
-  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
-]
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Finch
+from .forms import FeedingForm
 
 # Create your views here.
 def home(request):
@@ -14,6 +11,36 @@ def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', {
         'finches': finches
     })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    # instantiate FeedingForm to be rendered in detail.html
+    feeding_form = FeedingForm()
+    return render(request, 'finches/detail.html', {
+        'finch': finch, 'feeding_form': feeding_form
+    })
+
+class FinchCreate(CreateView):
+    model = Finch
+    fields = '__all__'
+# or you can add a List of what fields you want to display
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['breed', 'description', 'age']
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finches'
+
+def add_feeding(request, finch_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.finch_id = finch_id
+        new_feeding.save()
+    return redirect('detail', finch_id=finch_id)
